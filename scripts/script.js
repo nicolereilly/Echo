5084c6898baaca9f9bda43ccdaaed39640ba46e5
nicolereilly <111534190+nicolereilly@users.noreply.github.com>
2022-09-27 13:25:15 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -82,3 +82,16 @@ function nameDegree(name) {
     return "You would major in Marketing";
   }
 }
+
+var input = document.getElementById("myInput");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("discover your identity").click();
+  }
+});
